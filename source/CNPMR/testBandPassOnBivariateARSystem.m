d280b2805ccc1a4ebe6363b0385cde3b64d11d53
Axel Goblet <a.goblet@hotmail.com>
2017-05-07 15:31:04 +0200

    band pass filtering

--- source/CNPMR/testBandPassOnBivariateARSystem.m
+++ source/CNPMR/testBandPassOnBivariateARSystem.m
@@ -1,16 +1,51 @@
 % create multivariate AR model
-ynext = @(yprevious1,yprevious2,xprevious1,xprevious2)0.5*yprevious1-0.3*yprevious2+0.1*xprevious2+0.4*xprevious1*xprevious2+normrnd(0,.1);
-xnext = @(yprevious2,xprevious1,xprevious2)0.3*xprevious1-xprevious2-0.1*yprevious2+normrnd(0,.1);
+ynext = @(yprevious1,yprevious2,xprevious1,xprevious2)0.5*yprevious1-0.3*yprevious2+0.1*xprevious2+0.4*xprevious1*xprevious2+normrnd(0,0.1);
+xnext = @(yprevious2,xprevious1,xprevious2)0.3*xprevious1-xprevious2-0.1*yprevious2+normrnd(0,0.1);
 
 % generate 1000 samples
-y=zeros(1,10002);
-x=zeros(1,10002);
-for i=3:10002
+signalLength = 1000;
+historyLength = 2;
+y=zeros(1,signalLength + historyLength);
+x=zeros(1,signalLength + historyLength);
+for i=(historyLength+1):(signalLength + historyLength)
     y(i)=ynext(y(i-1),y(i-2),x(i-1),x(i-2));
     x(i)=xnext(y(i-2),x(i-1),x(i-2));
 end
-y=y(5000:10002);
-x=x(5000:10002);
+y=y((historyLength+1):(signalLength + historyLength));
+x=x((historyLength+1):(signalLength + historyLength));
 
-xtoycnpmr = CNPMR(y,x,[],1,3,1,1,[],false,0.05,[2,2.5],20)
-ytoxcnpmr = CNPMR(x,y,[],1,3,1,1,[],false,0.05,[2,2.5],20)
+% plot
+figure(1)
+subplot(2,2,1)
+plot(x)
+xlabel('t')
+title('signal x')
+subplot(2,2,2)
+plot(y)
+xlabel('t')
+title('signal y')
+subplot(2,2,3)
+xfft=fft(x);
+plot(frequencies,log(abs(xfft(1:(signalLength/2)))))
+xlabel('f')
+ylabel('log(A)')
+title('fft of x')
+subplot(2,2,4)
+yfft=fft(y);
+plot(frequencies,log(abs(yfft(1:(signalLength/2)))))
+xlabel('f')
+ylabel('log(A)')
+title('fft of y')
+
+% find causalities over frequencies
+bandwidth = 0.1;
+frequencies = linspace(bandwidth/1.99,10-bandwidth/1.99,500);
+causalities = arrayfun(@(frequency)CNPMR(y,x,[],1,3,1,1,[],false,0.5,[frequency-bandwidth/2,frequency+bandwidth/2],20), frequencies);
+figure(2)
+plot(frequencies,causalities)
+xlabel('f')
+ylabel('causality')
+title('causality x-->y over frequency')
+
+[c,~,s]=CNPMR(y,x,[],1,3,1,1,[],false,0.05,[4.45,4.55],20)
+[c,~,s]=CNPMR(y,x,[],1,3,1,1,[],false,0.05)
--- source/CNPMR/testBandPassOnDuffingOscillator.m
+++ source/CNPMR/testBandPassOnDuffingOscillator.m
@@ -6,16 +6,39 @@ k3=0.3;
 Ts=0.02;
 sigma=0.1;
 ynext = @(yprevious1,yprevious2,xprevious2)(2-0.5*Ts)*yprevious1-(1-0.5*Ts+Ts^2)*yprevious2-k3*Ts^2*yprevious2^3+Ts^2*xprevious2+normrnd(0,sigma);
-xnext = @(k) 5*sin(k*Ts)+normrnd(0,sigma);
+xnext = @(k) 5*sin(k/2)+normrnd(0,sigma);
 
 % generate 1000 samples
-y=zeros(1,1002);
-x=zeros(1,1002);
-for i=3:1002
-    [ynext(y(i-1),y(i-2),x(i-2)),xnext(i)]
-    
+signalLength = 1000;
+historyLength = 2;
+y=zeros(1,signalLength + historyLength);
+x=zeros(1,signalLength + historyLength);
+for i=(historyLength+1):(signalLength + historyLength)
     y(i)=ynext(y(i-1),y(i-2),x(i-2));
     x(i)=xnext(i);
 end
-y=y(3:1002);
-x=x(3:1002);
+y=y((historyLength+1):(signalLength + historyLength));
+x=x((historyLength+1):(signalLength + historyLength));
+
+% plot
+frequencies = linspace(0,1,signalLength/2);
+subplot(2,2,1)
+plot(x)
+xlabel('t')
+title('signal x')
+subplot(2,2,2)
+plot(y)
+xlabel('t')
+title('signal y')
+subplot(2,2,3)
+xfft=fft(x);
+plot(frequencies,log(abs(xfft(1:(signalLength/2)))))
+xlabel('f')
+ylabel('log(A)')
+title('fft of x')
+subplot(2,2,4)
+yfft=fft(y);
+plot(frequencies,log(abs(yfft(1:(signalLength/2)))))
+xlabel('f')
+ylabel('log(A)')
+title('fft of y')
