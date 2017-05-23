77b171efdc4fb5d26f41272217981e9b4592e6c9
Axel Goblet <a.goblet@hotmail.com>
2017-05-23 17:47:42 +0200

    henon map experiment finalized and presentation

--- source/CNPMR/testCNPMR4Henon.m
+++ source/CNPMR/testCNPMR4Henon.m
@@ -2,17 +2,35 @@
 iterations = 20000;
 %Do an amount of iterations to calculate Henon map
 c = [0,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0];
-x(1) = 0;
-y(1) = 0;
-x(2) = 0;
-y(2) = 0;
+x = zeros(1,iterations);
+y = zeros(1,iterations);
+
+figure
 for i = 1:size(c,2)
+    c(i)
     for t = 3:iterations
-        x(t)=1.4-(x(t-1)^2)+0.3*y(t-2);
+        x(t)=1.4-(x(t-1)^2)+0.3*x(t-2);
         y(t)=1.4-(c(i)*x(t-1)+(1-c(i))*y(t-1))*y(t-1)+0.1*y(t-2); 
     end
-     [causality,xR2,isSignificant,sensitivity] = CNPMR(x,y,[],1,3,1,1,[],true);
     
-    [causality,xR2,isSignificant,sensitivity] = CNPMR(y,x,[],1,3,1,1,[],false);
+    xtoy = zeros(1,25);
+    ytox = zeros(1,25);
+    for w = 1:25
+        w
+        indices = (w*750:((w+1)*750)-1);
+        ytox(w) = CNPMR(x(indices),y(indices),[],1,3,1,1,[],false,0.5);
+        xtoy(w) = CNPMR(y(indices),x(indices),[],1,3,1,1,[],false,0.5);
+    end
     
+    subplot(3,4,i)
+    plot(xtoy)
+    hold on
+    plot(ytox)
+    axis([1,25,0,1.5])
+    title(['c = '  num2str(c(i))])
+    xlabel('Window')
+    ylabel('Causality')
+    legend('x->y', 'y->x')
+    hold off
 end
+
